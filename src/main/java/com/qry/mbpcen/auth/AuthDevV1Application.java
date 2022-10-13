31502561eb57b7784c73d377f5b0179797d6c26a
minis24 <minis24@gmail.com>
2022-10-13 14:09:34 +0900

    test

--- src/main/java/com/qry/mbpcen/auth/AuthDevV1Application.java
+++ src/main/java/com/qry/mbpcen/auth/AuthDevV1Application.java
@@ -3,6 +3,8 @@ package com.qry.mbpcen.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class AuthDevV1Application {
 
