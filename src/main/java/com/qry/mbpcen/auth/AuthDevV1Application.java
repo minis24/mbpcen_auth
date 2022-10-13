64100188e68bf603671044e45c2759b0ddde3005
minis24 <minis24@gmail.com>
2022-10-13 14:02:50 +0900

    최초 등록

---
+++ src/main/java/com/qry/mbpcen/auth/AuthDevV1Application.java
@@ -0,0 +1,18 @@
+package com.qry.mbpcen.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthDevV1Application {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		SpringApplication.run(AuthDevV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/qry/mbpcen/auth/AuthDevV1ApplicationTests.java
@@ -0,0 +1,10 @@
+package com.qry.mbpcen.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthDevV1ApplicationTests {
+
+
+}
